50d644c92ecd32b9c749cc81b7926ef5774c0516
Bishwajit Karmaker <bish.karm123@gmail.com>
2023-01-10 23:42:40 +0100

    commit-2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -11,5 +11,31 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
-  
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurant_path, status: :see_other
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'restaurants', to: 'restaurant#index', as: :'restaurant lists'
-  get 'restaurants/:id', to: 'restaurant#show', as: :'restaurant details'
-  get 'restaurants/new', to: 'restaurant#new', as: :'create new restaurant'
+  get 'restaurants', to: 'restaurant#index', as: :restaurant_lists
+  get 'restaurants/:id', to: 'restaurant#show', as: :restaurant_details
+  get 'restaurants/new', to: 'restaurant#new', as: :create_restaurant
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch  'restaurants/:id', to: 'restaurants#update'
+  delete 'restaurants/:id', to: 'restaurant#destroy'
 end
