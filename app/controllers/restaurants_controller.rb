3e01b550d638172547fb983300812287e4a15679
Bishwajit Karmaker <bish.karm123@gmail.com>
2023-01-10 21:30:03 +0100

    commit-1

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,9 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'restaurants', to: 'restaurant#index', as: :restaurants
-  get 'restaurants/:id', to: 'restaurant#show', as: :restaurant
+  get 'restaurants', to: 'restaurant#index', as: :'restaurant lists'
+  get 'restaurants/:id', to: 'restaurant#show', as: :'restaurant details'
+  get 'restaurants/new', to: 'restaurant#new', as: :'create new restaurant'
 end
